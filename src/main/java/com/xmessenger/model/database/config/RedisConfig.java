6a227d260ef0e2471c2bec134a8c7a85eb55a5db
Andrii Olexandrovich <awispack@gmail.com>
2019-05-13 19:18:13 +0300

    Configuring Redis server - #9.

--- src/main/java/com/xmessenger/model/database/config/RedisConfig.java
+++ src/main/java/com/xmessenger/model/database/config/RedisConfig.java
@@ -18,7 +18,7 @@ public class RedisConfig {
     @Value("${spring.redis.port:6379}")
     private int redisPort;
 
-    @Value("${spring.redis.password}")
+    @Value("${spring.redis.password:samplepwd}")
     private String redisPassword;
 
     @Bean
