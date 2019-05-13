e8ee00a761510e3dd5560e8643602a33618c2cf1
Andrii Olexandrovich <awispack@gmail.com>
2019-05-13 19:11:47 +0300

    Configuring Redis server - #8.

--- src/main/java/com/xmessenger/model/database/config/RedisConfig.java
+++ src/main/java/com/xmessenger/model/database/config/RedisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
@@ -19,10 +18,11 @@ public class RedisConfig {
     @Value("${spring.redis.port:6379}")
     private int redisPort;
 
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        JedisPool jedisPool = new JedisPool();
-
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMinIdle(1);
         poolConfig.setMaxIdle(5);
@@ -30,18 +30,10 @@ public class RedisConfig {
         poolConfig.setTestOnReturn(true);
         poolConfig.setTestWhileIdle(true);
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(poolConfig);
-
-        jedisConnectionFactory.setHostName("ec2-52-209-84-212.eu-west-1.compute.amazonaws.com");
-        jedisConnectionFactory.setPort(9989);
-        jedisConnectionFactory.setPassword("p452d687123ce4e7cbb14c05739a596cbef15df379b6d88a26a100c512f69d8a4");
-
-//        jedisConnectionFactory.setHostName(this.redisHost);
-//        jedisConnectionFactory.setPort(this.redisPort);
+        jedisConnectionFactory.setHostName(this.redisHost);
+        jedisConnectionFactory.setPort(this.redisPort);
+        jedisConnectionFactory.setPassword(this.redisPassword);
         jedisConnectionFactory.setUsePool(true);
-
-        System.out.println(">>> REDIS HOST: " + this.redisHost);
-        System.out.println(">>> REDIS PORT: " + this.redisPort);
-
         return jedisConnectionFactory;
     }
 
