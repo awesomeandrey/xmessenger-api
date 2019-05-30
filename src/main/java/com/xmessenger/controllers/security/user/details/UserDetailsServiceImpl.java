f00c2369c2f416d9b514cf19cbdd4e6e7136258c
Andrii Olexandrovich <awispack@gmail.com>
2019-05-30 14:25:07 +0300

    Blocked deactivated users from logging in.

--- src/main/java/com/xmessenger/controllers/security/user/details/UserDetailsServiceImpl.java
+++ src/main/java/com/xmessenger/controllers/security/user/details/UserDetailsServiceImpl.java
@@ -17,6 +17,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         AppUser appUser = this.userService.lookupUser(username);
+        if (!appUser.isActive()) return null;
         return new User(appUser.getUsername(), appUser.getPassword(), appUser.getRoles());
     }
 }
