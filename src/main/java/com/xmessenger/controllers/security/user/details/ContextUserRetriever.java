5970ee56932caa829dd7c890ae5d7481c702ac56
Andrii Olexandrovich <awispack@gmail.com>
2019-06-11 20:26:49 +0300

    Changes to 'deleteUser' webservice.

--- src/main/java/com/xmessenger/controllers/security/user/details/ContextUserRetriever.java
+++ src/main/java/com/xmessenger/controllers/security/user/details/ContextUserRetriever.java
@@ -5,6 +5,7 @@ import com.xmessenger.model.services.core.user.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +19,12 @@ public class ContextUserRetriever {
 
     public AppUser getContextUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = (String) auth.getPrincipal();
+        String username = null;
+        if (auth.getPrincipal() instanceof String) {
+            username = (String) auth.getPrincipal();
+        } else if (auth.getPrincipal() instanceof UserDetails) {
+            username = ((UserDetails) auth.getPrincipal()).getUsername();
+        }
         return this.userDAO.getUserByUsername(username);
     }
 
