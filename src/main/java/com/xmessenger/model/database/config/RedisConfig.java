e85328a2854444ced8fbd47b7f9af89773fb9c54
Andrii Olexandrovich <awispack@gmail.com>
2019-06-16 17:56:31 +0300

    Cleaning up env variables for DB connections (redis + postgre).

--- src/main/java/com/xmessenger/model/database/config/RedisConfig.java
+++ src/main/java/com/xmessenger/model/database/config/RedisConfig.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
 
 import java.net.URI;
@@ -16,59 +13,22 @@ import java.net.URISyntaxException;
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
-    @Value("${spring.redis.host:localhost}")
-    private String redisHost;
-
-    @Value("${spring.redis.port:6379}")
-    private int redisPort;
-
-    @Value("${spring.redis.password:pwd}")
-    private String redisPassword;
-
-//    @Bean
-//    public JedisConnectionFactory jedisConnectionFactory() {
-//        JedisPoolConfig poolConfig = new JedisPoolConfig();
-//        poolConfig.setMinIdle(1);
-//        poolConfig.setMaxIdle(5);
-//        poolConfig.setTestOnBorrow(true);
-//        poolConfig.setTestOnReturn(true);
-//        poolConfig.setTestWhileIdle(true);
-//        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(poolConfig);
-//        jedisConnectionFactory.setHostName(this.redisHost);
-//        jedisConnectionFactory.setPort(this.redisPort);
-//        if (!this.redisHost.equals("localhost")) {
-//            jedisConnectionFactory.setPassword(this.redisPassword);
-//        }
-//        jedisConnectionFactory.setUsePool(true);
-//        return jedisConnectionFactory;
-//    }
-//
-//    @Bean
-//    public RedisTemplate<?, ?> redisTemplate() {
-//        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
-//        return template;
-//    }
+    @Value("${spring.redis.url}")
+    private String redisUrl;
 
     @Bean
     public JedisConnectionFactory jedisConnFactory() {
-
         try {
-//            String redistogoUrl = System.getenv("REDIS_URL");
-            String redistogoUrl = "redis://h:p9d75efa2403727d2242df39cf36543a72626b1a57f121d7b7ca8673ce3689d28@ec2-108-129-53-202.eu-west-1.compute.amazonaws.com:8959";
-            URI redistogoUri = new URI(redistogoUrl);
-
+            URI redisUri = new URI(this.redisUrl);
             JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
-
             jedisConnFactory.setUsePool(true);
-            jedisConnFactory.setHostName(redistogoUri.getHost());
-            jedisConnFactory.setPort(redistogoUri.getPort());
+            jedisConnFactory.setHostName(redisUri.getHost());
+            jedisConnFactory.setPort(redisUri.getPort());
             jedisConnFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
-            jedisConnFactory.setPassword(redistogoUri.getUserInfo().split(":", 2)[1]);
-
+            if (!redisUri.getHost().equals("localhost")) {
+                jedisConnFactory.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+            }
             return jedisConnFactory;
-
         } catch (URISyntaxException e) {
             e.printStackTrace();
             return null;
