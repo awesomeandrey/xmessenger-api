73b5e594c99deb67faa14c439dfb78019d627d26
Andrii Olexandrovich <awispack@gmail.com>
2019-06-16 18:42:24 +0300

    Configuring redis access.

--- src/main/java/com/xmessenger/model/database/config/RedisConfig.java
+++ src/main/java/com/xmessenger/model/database/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.xmessenger.model.database.config;
 
+import com.xmessenger.model.util.Utility;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,11 +26,22 @@ public class RedisConfig {
             jedisConnFactory.setHostName(redisUri.getHost());
             jedisConnFactory.setPort(redisUri.getPort());
             jedisConnFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
-            jedisConnFactory.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+            String password = this.getPassword(redisUri);
+            if (Utility.isNotBlank(password)) {
+                jedisConnFactory.setPassword(password);
+            }
             return jedisConnFactory;
         } catch (URISyntaxException e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private String getPassword(URI uri) {
+        String userInfo = uri.getUserInfo();
+        if (Utility.isBlank(userInfo)) return null;
+        String[] credentials = uri.getUserInfo().split(":", 2);
+        if (credentials.length < 1) return null;
+        return credentials[1];
     }
 }
