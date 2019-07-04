5ca35ca5de0a873593448a9cdbb8af1cf70c58bd
Andrii Olexandrovich <awispack@gmail.com>
2019-07-04 15:14:04 +0300

    Implemented custom exception handler.

---
+++ src/main/java/com/xmessenger/controllers/webservices/config/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.xmessenger.controllers.webservices.config;
+
+import com.google.gson.Gson;
+import com.xmessenger.controllers.webservices.exceptions.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public final ResponseEntity<Map<String, Object>> handleBadRequest(BadRequestException ex, WebRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getLocalizedMessage().split(":")[1].trim());
+        response.put("locale", request.getLocale());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/xmessenger/controllers/webservices/secured/resftul/admin/AdministrationController.java
+++ src/main/java/com/xmessenger/controllers/webservices/secured/resftul/admin/AdministrationController.java
@@ -13,6 +13,8 @@ import com.xmessenger.model.services.core.request.RequestService;
 import com.xmessenger.model.services.core.user.UserService;
 import com.xmessenger.model.services.core.user.credentials.decorators.RawCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
